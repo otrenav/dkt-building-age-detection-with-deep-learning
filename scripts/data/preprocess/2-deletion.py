9ab646731c15d62a643b3cb25a882547a82012c4
otrenav <omar@otrenav.com>
2018-09-13 02:57:58 -0500

    Delete observations without coordinates

--- scripts/data/preprocess/2-deletion.py
+++ scripts/data/preprocess/2-deletion.py
@@ -23,10 +23,16 @@ def existing_images_indexes():
     return [int(i.replace(".jpeg", "")) for i in os.listdir(IMAGES)]
 
 
+def delete_observations_with_errors(data):
+    return data[~data.lat.isnull()]
+
+
 if __name__ == "__main__":
     data = pd.read_csv(f"{OUTPUTS}/data.csv")
-    print(f"[+] DELETING OBSERVATIONS WITHOUT CORRESPONDING IMAGES...")
     n_start = data.shape[0]
+    print(f"[+] DELETING OBSERVATIONS WITH IMAGE RETRIEVAL ERRORS...")
+    data = delete_observations_with_errors(data)
+    print(f"[+] DELETING OBSERVATIONS WITHOUT CORRESPONDING IMAGES...")
     data = delete_observations_without_images(data)
     n_end = data.shape[0]
     print(f"    - Kept {n_end}/{n_start} observations ({n_end/n_start})")
